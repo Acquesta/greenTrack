be28d71fda7aaafa82a48f5e2a0142e21389edff
alexiaramalho <rm558385@fiap.com.br>
2024-11-14 18:42:32 -0300

    criando variaveis de cores

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      colors:{
+        fundo: "#141619",
+        verde: '#1EBF49',
+        azul: '#5FA4E6',
+        cinza: '#E5E5E5'
+      },
       fontFamily: {
         inter: ['Inter', 'sans-serif'], 
       },

1aef81eb759a5c812806cb2e72ef3d129223de6a
acquesta <lorenzzo.acquesta@gmail.com>
2024-11-22 17:42:51 -0300

    arrumando erros

--- src/components/dashboard/CardHome.jsx
+++ src/components/dashboard/CardHome.jsx
@@ -1,6 +1,7 @@
 function CardHome({ nome, width = '100%', height = '40vh', themeMode, children }) {
     return ( 
-        <article className={`my-5 rounded-lg bg-${themeMode == 'dark' ? 'fundo' : 'white'}`} style={{width: width}}>
+        <article className={`my-5 rounded-lg `} style={{width: width}}>
+            <div className="bg-cinzaCard"></div>
             <h2 className={`text-${themeMode == 'dark' ? 'white' : 'fundo'} text-3xl font-bold`}>{nome}</h2>
             <div style={{height: height}} className={`flex flex-col gap-3 mt-3 bg-${themeMode == 'dark' ? 'cinzaCard' : 'white'} rounded-lg py-5 shadow-md`}>
                 {children}
